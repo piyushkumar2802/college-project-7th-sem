806b417465845800130a15e1ac47357c27c6f562
piyush kumar <rkc2802@gmail.com>
2021-10-26 13:52:14 +0530

    second commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import {Button} from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>E-commmerce Project</h1>
+      <button>normal button</button>
+      <Button> button</Button>
+    </div>
+  );
+}
+
+export default App;
